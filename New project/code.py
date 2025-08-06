16ccbcc90ea605c99ff5eb2bab47b50e523d3e41
apple <aisworyapanda@gmail.com>
2025-08-06 10:49:15 +0530

    New Data added

--- New project/code.py
+++ New project/code.py
@@ -1,2 +1,6 @@
 name = input("Enter your name: ")
 print(name)
+user_age = input("Enter user age: ")
+years = int(user_age)
+months = years * 12
+print(f"Your age, {years}, is equal to {months} months.")
